984dc91830ebbedd543efd3dbbcf8b3fba7c2437
Saraansh <gandhisaraansh@gmail.com>
2026-07-11 11:52:15 +0530

    Time: 9 ms (82.85%), Space: 47.5 MB (76.09%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    ArrayList<Integer>[] graph;
+    boolean[] visited;
+
+    int nodes;
+    int degreeSum;
+
+    public int countCompleteComponents(int n, int[][] edges) {
+
+        graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+
+        visited = new boolean[n];
+
+        int complete = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
+
+                nodes = 0;
+                degreeSum = 0;
+
+                dfs(i);
+
+                int edgesInComponent = degreeSum / 2;
+
+                if (edgesInComponent == (nodes * (nodes - 1)) / 2) {
+                    complete++;
+                }
+            }
+        }
+
+        return complete;
+    }
+
+    void dfs(int node) {
+
+        visited[node] = true;
+        nodes++;
+
+        degreeSum += graph[node].size();
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
