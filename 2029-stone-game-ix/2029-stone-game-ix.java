1df891a8c94b2b8d0bc3d26aefc800b8bffa88fc
Saraansh <gandhisaraansh@gmail.com>
2026-08-16 23:52:50 +0530

    Time: 3 ms (100%), Space: 114.7 MB (64.86%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+
+        // Count stones based on remainder when divided by 3
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        // Even number of multiples of 3
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        // Odd number of multiples of 3
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
