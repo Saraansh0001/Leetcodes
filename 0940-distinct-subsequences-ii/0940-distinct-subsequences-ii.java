641f4e512b64eb16a827ae67af0e0ca5ffe934ac
Saraansh <gandhisaraansh@gmail.com>
2026-09-07 12:20:20 +0530

    Time: 4 ms (70.57%), Space: 43.7 MB (28.37%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -4,13 +4,10 @@ class Solution {
         int MOD = 1_000_000_007;
         int n = s.length();
 
-        // dp[i] = number of distinct subsequences
-        // including empty subsequence using first i characters
         long[] dp = new long[n + 1];
 
         dp[0] = 1;
 
-        // last[c] = dp value before the previous occurrence of c
         long[] last = new long[26];
 
         for (int i = 1; i <= n; i++) {
@@ -20,14 +17,11 @@ class Solution {
 
             dp[i] = (2 * dp[i - 1]) % MOD;
 
-            // Remove duplicates caused by previous occurrence of ch
             dp[i] = (dp[i] - last[c] + MOD) % MOD;
 
-            // For future occurrences of this character
             last[c] = dp[i - 1];
         }
 
-        // Remove empty subsequence
         return (int)((dp[n] - 1 + MOD) % MOD);
     }
 }
