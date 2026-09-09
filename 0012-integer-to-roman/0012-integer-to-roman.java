6cb0573aff1eb73716397da25fafcd23b6c4e944
Saraansh <gandhisaraansh@gmail.com>
2026-09-09 20:10:51 +0530

    Time: 3 ms (99.8%), Space: 46.3 MB (77.81%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,64 +1,28 @@
 class Solution {
     public String intToRoman(int num) {
 
-        String ans = "";
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
 
-        while (num != 0) {
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
 
-            if (num >= 1000) {
-                ans += 'M';
-                num -= 1000;
-            }
-            else if (num >= 900) {
-                ans += "CM";
-                num -= 900;
-            }
-            else if (num >= 500) {
-                ans += 'D';
-                num -= 500;
-            }
-            else if (num >= 400) {
-                ans += "CD";
-                num -= 400;
-            }
-            else if (num >= 100) {
-                ans += 'C';
-                num -= 100;
-            }
-            else if (num >= 90) {
-                ans += "XC";
-                num -= 90;
-            }
-            else if (num >= 50) {
-                ans += 'L';
-                num -= 50;
-            }
-            else if (num >= 40) {
-                ans += "XL";
-                num -= 40;
-            }
-            else if (num >= 10) {
-                ans += 'X';
-                num -= 10;
-            }
-            else if (num >= 9) {
-                ans += "IX";
-                num -= 9;
-            }
-            else if (num >= 5) {
-                ans += 'V';
-                num -= 5;
-            }
-            else if (num >= 4) {
-                ans += "IV";
-                num -= 4;
-            }
-            else {
-                ans += 'I';
-                num -= 1;
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+
+            while (num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
             }
         }
 
-        return ans;
+        return ans.toString();
     }
 }
