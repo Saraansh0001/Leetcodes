153feda04e743f88ff7a7ccbb54863f4d688dcde
Saraansh <gandhisaraansh@gmail.com>
2026-09-09 19:56:18 +0530

    Time: 1 ms (99.36%), Space: 42.1 MB (97.44%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,19 +1,25 @@
 class Solution {
     public long countCommas(long n) {
 
-        if (n < 1000) return 0;
-
         long count = 0;
-        long temp = n;
-        long power = 1000;
 
-        while (temp >= 1000) {
+        if (n >= 1_000)
+            count += n - 1_000 + 1;
 
-            count += n - power + 1;
+        if (n >= 1_000_000)
+            count += n - 1_000_000 + 1;
 
-            temp /= 1000;
-            power *= 1000;
-        }
+        if (n >= 1_000_000_000)
+            count += n - 1_000_000_000L + 1;
+
+        if (n >= 1_000_000_000_000L)
+            count += n - 1_000_000_000_000L + 1;
+
+        if (n >= 1_000_000_000_000_000L)
+            count += n - 1_000_000_000_000_000L + 1;
+
+        if (n >= 1_000_000_000_000_000_000L)
+            count += n - 1_000_000_000_000_000_000L + 1;
 
         return count;
     }
