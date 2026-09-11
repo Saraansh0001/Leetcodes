86b5f68cb2b7d5dd427f3ad054bafc47fd851500
Saraansh <gandhisaraansh@gmail.com>
2026-09-11 14:33:11 +0530

    Time: 1 ms (100%), Space: 43.9 MB (93.07%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -9,22 +9,18 @@ class Solution {
 
         int ans = 0;
 
-        // Choose the last digit (must be even)
         for (int last = 0; last <= 8; last += 2) {
 
             if (freq[last] == 0) continue;
 
-            // Use one occurrence of last digit
             freq[last]--;
 
-            // Choose first digit (cannot be 0)
             for (int first = 1; first <= 9; first++) {
 
                 if (freq[first] == 0) continue;
 
                 freq[first]--;
 
-                // Choose the middle digit
                 for (int middle = 0; middle <= 9; middle++) {
                     if (freq[middle] > 0) {
                         ans++;
