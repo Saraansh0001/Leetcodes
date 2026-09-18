17b2c48461e36bfbd5e513c2010d80de35e08848
Saraansh <gandhisaraansh@gmail.com>
2026-09-18 20:48:47 +0530

    Time: 8 ms (96.16%), Space: 47.9 MB (78.26%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -6,10 +6,8 @@ class Solution {
         int[] first = new int[26];
         int[] last = new int[26];
 
-        // Initialize first positions
         Arrays.fill(first, -1);
 
-        // Find first and last occurrence of every character
         for (int i = 0; i < n; i++) {
             int c = s.charAt(i) - 'a';
 
@@ -22,7 +20,6 @@ class Solution {
 
         List<int[]> intervals = new ArrayList<>();
 
-        // Try to create a valid interval for every character
         for (int c = 0; c < 26; c++) {
 
             if (first[c] == -1)
@@ -37,13 +34,11 @@ class Solution {
 
                 int current = s.charAt(i) - 'a';
 
-                // This character started before our interval
                 if (first[current] < l) {
                     valid = false;
                     break;
                 }
 
-                // Need to include all occurrences of this character
                 r = Math.max(r, last[current]);
             }
 
@@ -52,7 +47,6 @@ class Solution {
             }
         }
 
-        // Sort by ending position
         intervals.sort((a, b) -> Integer.compare(a[1], b[1]));
 
         List<String> ans = new ArrayList<>();
@@ -64,7 +58,6 @@ class Solution {
             int l = interval[0];
             int r = interval[1];
 
-            // Non-overlapping
             if (l > prevEnd) {
                 ans.add(s.substring(l, r + 1));
                 prevEnd = r;
