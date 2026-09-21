8bf18a557f36bb3d7121f32883d22ece1ae98463
Saraansh <gandhisaraansh@gmail.com>
2026-09-21 18:52:24 +0530

    Time: 5 ms (38.41%), Space: 45.5 MB (9.29%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,49 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> n1 = new HashMap<>();
+        HashMap<Integer, Integer> n2 = new HashMap<>();
+
+        int size = Math.max(nums1.length, nums2.length);
+        int[] result = new int[size];
 
         for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            n1.put(num, n1.getOrDefault(num, 0) + 1);
         }
 
-        int[] ans = new int[Math.min(nums1.length, nums2.length)];
-        int index = 0;
-
         for (int num : nums2) {
+            n2.put(num, n2.getOrDefault(num, 0) + 1);
+        }
 
-            if (map.containsKey(num) && map.get(num) > 0) {
+        int fSize = 0;
 
-                ans[index++] = num;
+        for (int num : nums1) {
 
-                map.put(num, map.get(num) - 1);
+            if (n2.containsKey(num)) {
+
+                while (n1.containsKey(num) && n2.containsKey(num)) {
+
+                    result[fSize] = num;
+                    fSize++;
+
+                    n1.put(num, n1.get(num) - 1);
+                    n2.put(num, n2.get(num) - 1);
+
+                    if (n1.get(num) == 0)
+                        n1.remove(num);
+
+                    if (n2.get(num) == 0)
+                        n2.remove(num);
+                }
             }
         }
 
-        return Arrays.copyOf(ans, index);
+        int[] finalArray = new int[fSize];
+
+        for (int i = 0; i < fSize; i++) {
+            finalArray[i] = result[i];
+        }
+
+        return finalArray;
     }
 }
