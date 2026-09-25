f2a8d8aebf9cf28dd00ab220fb69ea1fd509da65
Saraansh <gandhisaraansh@gmail.com>
2026-09-25 18:54:18 +0530

    Time: 31 ms (8%), Space: 53.1 MB (6.4%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -8,7 +8,6 @@ class Solution {
 
     private void dfs(String exp) {
 
-        // No braces left → complete word
         int close = exp.indexOf('}');
 
         if (close == -1) {
@@ -16,14 +15,12 @@ class Solution {
             return;
         }
 
-        // Find matching opening brace
         int open = exp.lastIndexOf('{', close);
 
         String before = exp.substring(0, open);
         String inside = exp.substring(open + 1, close);
         String after = exp.substring(close + 1);
 
-        // Try every option inside {}
         for (String choice : inside.split(",")) {
             dfs(before + choice + after);
         }
